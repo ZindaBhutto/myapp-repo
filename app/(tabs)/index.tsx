063f171ddabb81962451cf88c30901f6625481ec
shakirahafeez456 <56725618+shakirahafeez456@users.noreply.github.com>
2024-10-20 15:33:33 +0500

    first commit

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -17,7 +17,7 @@ export default function HomeScreen() {
         />
       }>
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
+        <ThemedText type="title">Welcome!!!!</ThemedText>
         <HelloWave />
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
@@ -69,3 +69,8 @@ const styles = StyleSheet.create({
     position: 'absolute',
   },
 });
+
+
+
+
+
